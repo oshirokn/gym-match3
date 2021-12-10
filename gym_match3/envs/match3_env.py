1adb84e775ae6de27f3684a560c64b80a0c95668
Ken Oshiro <oshiro.kn@gmail.com>
2021-12-10 14:55:33 +0200

    Update match3_env.py

--- gym_match3/envs/match3_env.py
+++ gym_match3/envs/match3_env.py
@@ -83,8 +83,8 @@ class Match3Env(gym.Env):
                         continue
                         
         print(list(actions)[0][0].get_coord()[0])
-        print(list(actions).sort(key=lambda tup: tup[0].get_coord()[0]))
-        return list(actions).sort(key=lambda tup: tup[0].get_coord()[0])
+        print(list(actions).sort(key=lambda x:x[0][0].get_coord()[0]))
+        return list(actions).sort(key=lambda x:x[0][0].get_coord()[0])
 
     def __get_action(self, ind):
         return self.__match3_actions[ind]
