852bc0e2f8a478676fda177ac8e3091e7dc7c65c
Ken Oshiro <oshiro.kn@gmail.com>
2021-12-10 14:31:58 +0200

    Update match3_env.py

--- gym_match3/envs/match3_env.py
+++ gym_match3/envs/match3_env.py
@@ -82,7 +82,7 @@ class Match3Env(gym.Env):
                     except OutOfBoardError:
                         continue
                         
-        print(list(actions)[0])
+        print(list(actions)[0][0])
         print(list(actions).sort(key=lambda tup: tup[0].get_coord()[0]))
         return list(actions)
 
