55e55594c843a8bbed5d3363baddaac96c43c812
Ken Oshiro <oshiro.kn@gmail.com>
2021-12-15 13:55:09 +0200

    Update match3_env.py

--- gym_match3/envs/match3_env.py
+++ gym_match3/envs/match3_env.py
@@ -101,7 +101,11 @@ class Match3Env(gym.Env):
             for i in range(rows-1):
                 actions.append((Point(i,j), Point(i+1, j)))
 
-
+        possibleMoves = self.__get_possible_moves(self)
+        print(possibleMoves)
+        print(actions[0]
+        #for i in range(actions):
+        #    if actions[i]
 
         return actions
 
