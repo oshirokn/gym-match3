8fe1ee96a554a22986d0d5a5c332b79c3cdf2fa0
Ken Oshiro <oshiro.kn@gmail.com>
2021-12-16 16:00:03 +0200

    Update match3_env.py

--- gym_match3/envs/match3_env.py
+++ gym_match3/envs/match3_env.py
@@ -93,18 +93,14 @@ class Match3Env(gym.Env):
         actions = []
         
         possible_moves = list(self.__game._Game__get_possible_moves())
-        print(possible_moves)
-        print(possible_moves[0])
-        print(possible_moves[0][0])
-        print(possible_moves[0][1])
-        print(possible_moves[0][2])
+        #print(possible_moves)
+        #print(possible_moves[0])
+        #print(possible_moves[0][0])
+        #print(possible_moves[0][1])
         
-
         for i in range(len(possible_moves)):
             actions.append((possible_moves[i][0],possible_moves[i][1]))
-        print(actions)
-        print(actions[0])
-        print(type(actions[0][0]))
+
         #rows, cols = self.__game.board.board_size
 
         #for i in range(rows):
