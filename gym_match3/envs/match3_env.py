2b87cc3ba3505671be9fb9b4032f3bbe6f3f3069
Ken Oshiro <oshiro.kn@gmail.com>
2021-12-16 15:28:38 +0200

    Update match3_env.py

--- gym_match3/envs/match3_env.py
+++ gym_match3/envs/match3_env.py
@@ -97,7 +97,8 @@ class Match3Env(gym.Env):
         for i in range(len(possible_moves)):
             actions.append(possible_moves[i])
         print(actions)
-        
+        print(actions[0])
+        print(type(actions[0]))
         #rows, cols = self.__game.board.board_size
 
         #for i in range(rows):
